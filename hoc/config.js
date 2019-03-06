0f84600554d63145c1f9093086c58854be8dff14
OctavioDuart <36246332+OctavioDuart@users.noreply.github.com>
2019-03-06 02:39:15 -0300

    Config

--- hoc/config.js
+++ hoc/config.js
@@ -1,7 +1,7 @@
-const key_address = `YOUR KEY API`;
+const key_address = `&sensor=false&key=[YOUR KEY API]`;
 const route_address = `https://maps.googleapis.com/maps/api/geocode/json?latlng=`;
 const route_gas_station = `https://maps.googleapis.com/maps/api/place/search/json?location=`;
-const key_gas_station = `YOUR KEY API`;
+const key_gas_station = `&radius=10000&type=gas_station&key=[YOUR KEY API]`;
 
 module.exports = {
     route_gas_station,
